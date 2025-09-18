21e1ed1bfd0d7ac5cd65fcb3f5fd97725daabb57
aibert-enni <einshtein_qwer@mail.ru>
2025-09-18 23:36:38 +0500

    chore(storage): add test bucket for tests in minio;

--- media_service/tests/fixtures/__init__.py
+++ media_service/tests/fixtures/__init__.py
@@ -1,5 +1,6 @@
+from .setup import setup
 from .db import db_session, engine
 from .services import media_service
-from .setup import setup
+
 
 __all__ = ["setup", "engine", "db_session", "media_service"]
--- media_service/tests/fixtures/setup.py
+++ media_service/tests/fixtures/setup.py
@@ -1,8 +1,9 @@
 import pytest_asyncio
 
 from app.core.settings import settings
-
+from app.services.s3_service import s3_client
 
 @pytest_asyncio.fixture(scope="session", autouse=True)
 async def setup():
     settings.file.default_expire_seconds = 5
+    s3_client.bucket_name = "test"
