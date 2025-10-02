472dc28489c70d7f2edcc0e2d1ee329795f4fb00
aibert-enni <einshtein_qwer@mail.ru>
2025-10-03 00:21:31 +0500

    feat: add prometheus and grafana to api-gateway;

--- api-gateway/api/main.py
+++ api-gateway/api/main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 from app.core.setup import setup    
 from api.exceptions.exception_handlers import setup_exception_handlers
 from api.routers.v1 import router as v1_router
+from prometheus_fastapi_instrumentator import Instrumentator
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncIterator[None]:
@@ -14,6 +15,18 @@ async def lifespan(app: FastAPI) -> AsyncIterator[None]:
 app = FastAPI(
     root_path="/api", title="API Gateway", version="0.0.1", lifespan=lifespan
 )
+
+instrumentator = Instrumentator(
+    should_instrument_requests_inprogress=True,
+    should_group_status_codes=False,
+    excluded_handlers=[
+        "/docs", "/metrics", "/openapi.json", ".*admin.*", ".*health.*"
+    ],
+    inprogress_name="inprogress",
+    inprogress_labels=True
+)
+
+instrumentator.instrument(app).expose(app)
 
 setup_exception_handlers(app)
 
