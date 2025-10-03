0862685a1d4f095ebb823524abd4959cb271f2bc
aibert-enni <einshtein_qwer@mail.ru>
2025-10-03 13:35:43 +0500

    feat: add loki and promtail to api-gateway;

--- api-gateway/app/core/setup.py
+++ api-gateway/app/core/setup.py
@@ -1,4 +1,13 @@
+import logging
+import os
 from .settings import log_settings
 
 def setup():
+    os.makedirs("logs", exist_ok=True)
+    logging.basicConfig(
+        filename="logs/app.log",
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    )
+
     log_settings()
